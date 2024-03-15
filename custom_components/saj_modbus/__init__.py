59feea0e61cbf8145b3f3fd45217654f619c0eab
Holger Freier <holger.freier@gmail.com>
2024-03-15 14:37:56 +0100

    Add files via upload

---
+++ custom_components/saj_modbus/__init__.py
@@ -0,0 +1,95 @@
+"""The SAJ Modbus Integration."""
+import asyncio
+import logging
+
+
+
+# Konfigurieren des Root-Loggers
+#logging.basicConfig(level=logging.DEBUG)
+
+# Konfigurieren des Loggers für pymodbus, um Debug-Meldungen zu sehen
+#pymodbus_logger = logging.getLogger('pymodbus')
+#pymodbus_logger.setLevel(logging.DEBUG)
+
+# Konfigurieren des Loggers für Ihre Custom Component
+#custom_component_logger = logging.getLogger('custom_components.saj_modbus')
+#custom_component_logger.setLevel(logging.DEBUG)
+
+
+
+import homeassistant.helpers.config_validation as cv
+import voluptuous as vol
+from homeassistant.config_entries import ConfigEntry
+from homeassistant.const import CONF_HOST, CONF_NAME, CONF_PORT, CONF_SCAN_INTERVAL
+from homeassistant.core import HomeAssistant
+
+from .const import (
+    DEFAULT_NAME,
+    DEFAULT_SCAN_INTERVAL,
+    DOMAIN,
+)
+from .hub import SAJModbusHub
+
+_LOGGER = logging.getLogger(__name__)
+
+SAJ_MODBUS_SCHEMA = vol.Schema(
+    {
+        vol.Optional(CONF_NAME, default=DEFAULT_NAME): cv.string,
+        vol.Required(CONF_HOST): cv.string,
+        vol.Required(CONF_PORT): cv.string,
+        vol.Optional(
+            CONF_SCAN_INTERVAL, default=DEFAULT_SCAN_INTERVAL
+        ): cv.positive_int,
+    }
+)
+
+CONFIG_SCHEMA = vol.Schema(
+    {DOMAIN: vol.Schema({cv.slug: SAJ_MODBUS_SCHEMA})}, extra=vol.ALLOW_EXTRA
+)
+
+PLATFORMS = ["sensor"]
+
+
+async def async_setup(hass, config):
+    """Set up the SAJ modbus component."""
+    hass.data[DOMAIN] = {}
+    return True
+
+
+async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry):
+    """Set up a SAJ mobus."""
+    host = entry.data[CONF_HOST]
+    name = entry.data[CONF_NAME]
+    port = entry.data[CONF_PORT]
+    scan_interval = entry.data[CONF_SCAN_INTERVAL]
+
+    _LOGGER.debug("Setup %s.%s", DOMAIN, name)
+
+    hub = SAJModbusHub(hass, name, host, port, scan_interval)
+    await hub.async_config_entry_first_refresh()
+
+    """Register the hub."""
+    hass.data[DOMAIN][name] = {"hub": hub}
+
+    for component in PLATFORMS:
+        hass.async_create_task(
+            hass.config_entries.async_forward_entry_setup(entry, component)
+        )
+    return True
+
+
+async def async_unload_entry(hass, entry):
+    """Unload SAJ mobus entry."""
+    unload_ok = all(
+        await asyncio.gather(
+            *[
+                hass.config_entries.async_forward_entry_unload(entry, component)
+                for component in PLATFORMS
+            ]
+        )
+    )
+    if not unload_ok:
+        return False
+
+    hass.data[DOMAIN].pop(entry.data["name"])
+    return True
