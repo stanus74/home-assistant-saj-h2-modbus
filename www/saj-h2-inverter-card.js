7e5af03105e0825f7ee033e54fe051df96d4cab1
stanus74 <holger.freier@gmail.com>
2025-10-20 19:15:42 +0200

    up

--- www/saj-h2-inverter-card.js
+++ www/saj-h2-inverter-card.js
@@ -7,7 +7,7 @@
  * - Protects specific input interactions (time, range) from disruptive re-renders.
  *
  * @author stanu74 
- * @version 1.1.5-2
+ * @version 1.1.5
  */
 
 class SajH2InverterCard extends HTMLElement {
